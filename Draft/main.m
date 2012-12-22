cfb27c219579970d4478d5189899e5831e1fc613
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2012-12-22 00:11:05 -0500

    Initial Commit

---
+++ Draft/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Draft
+//
+//  Created by Jeremy on 2012-12-22.
+//  Copyright (c) 2012 Jeremy W. Sherman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
